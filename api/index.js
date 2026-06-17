cb4f39fb63e0215022bc94d98b6c6576abac205c
Lagos Rhythm Dev <dev@lagosrhythm.com>
2026-06-17 01:03:47 +0000

    Try .js extension for API function

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-export default function handler(req: any, res: any) {
+export default function handler(req, res) {
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.end(JSON.stringify({ ok: true }));
 }
